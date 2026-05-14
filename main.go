db180ea3dbbeb023c4c007f0e50a725f23ddd73a
asmagaa <geraldciekniedripem@gmail.com>
2026-05-14 23:44:44 +0200

    added limit (up to 100 fetched repos) and fix errors with http

--- main.go
+++ main.go
@@ -33,7 +33,18 @@ type JSONData struct {
 }
 
 func main() {
-	res, err := http.Get("https://api.github.com/search/repositories?q=stars:>=5000+language:c&sort=stars&order=desc")
+	var limit int
+
+	fmt.Println("How many repository search results would you like to see?")
+	fmt.Scanln(&limit)
+
+	if limit <= 0 || limit > 100 {
+		log.Fatal("Limit must be between 1 and 100.")
+	}
+
+	url := fmt.Sprintf("https://api.github.com/search/repositories?q=stars:>=5000+language:c&sort=stars&order=desc&per_page=%d", limit)
+	
+	res, err := http.Get(url)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -58,17 +69,22 @@ func main() {
 		log.Fatal(err)
 	}
 
-	printData(data)
+	printData(data, limit)
 }
 
-func printData(data JSONData) {
+func printData(data JSONData, limit int) {
 	log.Printf("Repositories found: %d", data.Count)
 	const format = "%v\t%v\t%v\t%v\t\n"
 	tw := new(tabwriter.Writer).Init(os.Stdout, 0, 8, 2, ' ', 0)
 
 	fmt.Fprintf(tw, format, "Repo", "Stars", "Created at", "Description")
 
-	for _, i := range data.Items {
+	if limit > len(data.Items) {
+		limit = len(data.Items)
+	}
+	items := data.Items[:limit]
+
+	for _, i := range items {
 		desc := i.Description
 		if len(desc) > 35 {
 			desc = string(desc[:35]) + "..."
